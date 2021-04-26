5b613bca3ddb3afa61bb76e4f03886fcb2b72105
Renato Bessa <bessache@outlook.com>
2021-04-26 08:58:39 -0300

    commit pagina de poste funcinal até header, falha ao renderizar o content

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client';
-import { useRouter } from 'next/router';
+
 import { FiUser, FiCalendar, FiClock } from 'react-icons/fi';
+import { RichText } from 'prismic-dom';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -42,20 +43,27 @@ export default function Post({ post }: PostProps): JSX.Element {
       <main className={commonStyles.container}>
         <article className={styles.post}>
           <h1>{post.data.title}</h1>
-          <footer>
-            <div>
-              <FiCalendar size={20} />
-              <time>{post.first_publication_date}</time>
-            </div>
-            <div>
-              <FiUser size={20} />
-              <span>{post.data.author}</span>
-            </div>
-            <div>
+          <div>
+            <ul>
+              <li>
+                <FiCalendar size={20} />
+                <time>{post.first_publication_date}</time>
+              </li>
+              <li>
+                <FiUser size={20} />
+                <span>{post.data.author}</span>
+              </li>
+            </ul>
+            <span>
               <FiClock size={20} />
-              <span> 10 min</span>
+              10 min
+            </span>
+          </div>
+          {post.data.content.map(content => (
+            <div key={content.heading}>
+              <strong>{content.heading}</strong>
             </div>
-          </footer>
+          ))}
         </article>
       </main>
     </>
@@ -66,7 +74,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
 
   const posts = await prismic.query([
-    Prismic.predicates.at('documents.type', 'posts'),
+    Prismic.predicates.at('document.type', 'posts'),
   ]);
 
   const paths = posts.results.map(post => {
@@ -90,7 +98,13 @@ export const getStaticProps: GetStaticProps<PostProps> = async ({ params }) => {
 
   const post = {
     slug: response.uid,
-    first_publication_date: response.first_publication_date,
+    first_publication_date: new Date(
+      response.first_publication_date
+    ).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
     data: {
       title: response.data.title,
       subtitle: response.data.subtitle,
@@ -101,7 +115,7 @@ export const getStaticProps: GetStaticProps<PostProps> = async ({ params }) => {
       content: response.data.content.map(content => {
         return {
           heading: content.heading,
-          body: [...content.body],
+          body: RichText.asHtml([...content.body]),
         };
       }),
     },
